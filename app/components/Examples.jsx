e224a615e2cf9ba4fe2ad5cb8652e8d3553793fc
Sotiris Valogiannis <sotirisvalogiannis@macbooks-MacBook-Pro.local>
2016-08-12 01:20:49 +0300

    Rename example page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var Examples = function(props) {
   return (
     <div>
-      <h3>Examples Component!</h3>
+      <h3>Examples</h3>
       <p>Welcome to our page</p>
     </div>
   )
